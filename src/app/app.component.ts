f742fd3f421b5e257f35055983e8c5b197505836
Sangeetha-Personal <sangeetha23121999@gmail.com>
2024-11-09 10:10:20 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,55 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    CommonModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatCheckboxModule,
+    MatButtonModule,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'materialForm';
+  form!: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      name: ['', [Validators.required, Validators.pattern('[a-zA-Z]*')]],
+      age: ['', [Validators.required, Validators.pattern('[0-9]*')]],
+      city: ['', Validators.required],
+      agree: [false, Validators.requiredTrue],
+      gender: ['', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.form.valid) {
+      console.log('Form submitted successfully:', this.form.value);
+      alert("Registered successfully!!!");
+      this.resetForm();
+    } else {
+      console.error('Form submission failed. Please fill out all fields correctly.');
+    }
+  }
+
+  resetForm(): void {
+    this.form.reset();
+  }
 }
